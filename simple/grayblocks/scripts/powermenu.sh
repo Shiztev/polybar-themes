1bfb3bd4d8b35c8f277f98b080655371ab30e43f
Stevie Alvarez <stevie.6strings@gmail.com>
2024-07-31 21:11:06 -0700

    added my config

--- simple/grayblocks/scripts/powermenu.sh
+++ simple/grayblocks/scripts/powermenu.sh
@@ -69,7 +69,8 @@ case $chosen in
 		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
 			mpc -q pause
 			amixer set Master mute
-			systemctl suspend
+			i3lock && systemctl hibernate
+			#systemctl suspend
 		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
 			exit 0
         else
